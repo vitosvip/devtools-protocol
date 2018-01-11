1935acbf039d965ce05c41a1d9aa2a565c9a9244
Paul Irish <paul.irish@gmail.com>
2018-01-11 15:36:11 -0800

    Updates

--- bower_components/webcomponentsjs/custom-elements-es5-adapter.js
+++ bower_components/webcomponentsjs/custom-elements-es5-adapter.js
@@ -1,16 +1,1 @@
-(function () {
-'use strict';
-
-(()=>{'use strict';if(!window.customElements)return;const a=window.HTMLElement,b=window.customElements.define,c=window.customElements.get,d=new Map,e=new Map;let f=!1,g=!1;window.HTMLElement=function(){if(!f){const a=d.get(this.constructor),b=c.call(window.customElements,a);g=!0;const e=new b;return e}f=!1;},window.HTMLElement.prototype=a.prototype;Object.defineProperty(window,'customElements',{value:window.customElements,configurable:!0,writable:!0}),Object.defineProperty(window.customElements,'define',{value:(c,h)=>{const i=h.prototype,j=class extends a{constructor(){super(),Object.setPrototypeOf(this,i),g||(f=!0,h.call(this)),g=!1;}},k=j.prototype;j.observedAttributes=h.observedAttributes,k.connectedCallback=i.connectedCallback,k.disconnectedCallback=i.disconnectedCallback,k.attributeChangedCallback=i.attributeChangedCallback,k.adoptedCallback=i.adoptedCallback,d.set(h,c),e.set(c,h),b.call(window.customElements,c,j);},configurable:!0,writable:!0}),Object.defineProperty(window.customElements,'get',{value:(a)=>e.get(a),configurable:!0,writable:!0});})();
-
-/**
-@license
-Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
-}());
+(function(){'use strict';(()=>{if(!window.customElements)return;const j=window.HTMLElement,a=window.customElements.define,h=window.customElements.get,l=new Map,b=new Map;let c=!1,d=!1;window.HTMLElement=function(){if(!c){const c=l.get(this.constructor),a=h.call(window.customElements,c);d=!0;const b=new a;return b}c=!1},window.HTMLElement.prototype=j.prototype,Object.defineProperty(window,'customElements',{value:window.customElements,configurable:!0,writable:!0}),Object.defineProperty(window.customElements,'define',{value:(e,f)=>{const g=f.prototype,h=class extends j{constructor(){super(),Object.setPrototypeOf(this,g),d||(c=!0,f.call(this)),d=!1}},i=h.prototype;h.observedAttributes=f.observedAttributes,i.connectedCallback=g.connectedCallback,i.disconnectedCallback=g.disconnectedCallback,i.attributeChangedCallback=g.attributeChangedCallback,i.adoptedCallback=g.adoptedCallback,l.set(f,e),b.set(e,f),a.call(window.customElements,e,h)},configurable:!0,writable:!0}),Object.defineProperty(window.customElements,'get',{value:(c)=>b.get(c),configurable:!0,writable:!0})})()})();
